f7631a2a2bd3c68f7641c8131edaa8c490515aa7
wata <watashimokia@gmail.com>
2024-11-17 00:29:36 +0900

    making friction system
    I know wrong. about conflict == friction.
    So I exchange all conflict to friction

--- Hope'sPeriod/Assets/Scripts/Battle/PlayerMovement/PlayerMovement.cs
+++ Hope'sPeriod/Assets/Scripts/Battle/PlayerMovement/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Mono.Cecil.Cil;
 using System;
 using UnityEngine;
 
@@ -27,9 +28,55 @@ public class PlayerMovement
 
     }
 
-    Vector2 CalculateConflict(Vector2 velocity, float conflict) {
+    Vector2 CalculateFrictionPercent(Direction frictionDirection, Vector2 velocity, float fritionPower, float trashhold) {
 
-        velocity *= conflict;
+        fritionPower = Mathf.Abs(fritionPower);
+
+        foreach(Direction checkDirection in Enum.GetValues(typeof(Direction))) {
+
+            if(DirectionInfo.Contain(frictionDirection, checkDirection)) {
+
+                int symbol = 0;
+
+                Vector2 checkDirectionVector = DirectionInfo.ConvertVector(checkDirection); 
+
+                if(checkDirection >= Direction.LEFT) {
+
+                    //* I can't decide between those. but many people say second is more well. (I can't admit it)
+                    //symbol = (checkDirectionVector.x >= 0 ? 1 : -1);
+                    symbol = Convert.ToInt32(Mathf.Sign(checkDirectionVector.x));
+                    if(symbol * velocity.x > 0) {
+
+                        if(velocity.x <= trashhold) {
+
+                            velocity.x = 0;
+                        }
+
+                        else {
+
+                            velocity.x *= fritionPower;
+                        }
+                    }
+                }
+
+                else {
+
+                    symbol = Convert.ToInt32(Mathf.Sign(checkDirectionVector.y));
+                    if (symbol * velocity.y > 0) {
+
+                        if (velocity.y <= trashhold) {
+
+                            velocity.y = 0;
+                        }
+
+                        else {
+
+                            velocity.y *= fritionPower;
+                        }
+                    }
+                }
+            }
+        }
 
         return velocity;
     }
