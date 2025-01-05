a65bb28775353b10423b78422a4998955a495d59
wata548 <watashimokia@gmail.com>
2025-01-05 17:33:17 +0900

    test map

---
+++ Hope'sPeriod/Assets/Scripts/Test/ShowState.cs
@@ -0,0 +1,15 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class ShowState : MonoBehaviour {
+    
+    private TMP_Text text;
+    private void Awake() {
+        text = GetComponent<TMP_Text>();
+    }
+
+    void Update() {
+        text.text = GameFSM.Instance.State.ToString();
+    }
+}
